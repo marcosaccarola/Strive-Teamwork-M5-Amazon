b40fba3c594c64e3736f1a1537c12158592137c0
Bimal27 <bimalsah1996@gmail.com>
2021-09-20 13:30:07 +0200

    error handled and validation set

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 import express from 'express'
+import {badRequestErrorHandler,notFoundErrorHandler, forbiddenErrorHandler, genericServerErrorHandler } from './errorHandlers.js'
 
 const server=express()
 const PORT=3001
+
+server.use(badRequestErrorHandler)
+server.use(notFoundErrorHandler)
+server.use(forbiddenErrorHandler)
+server.use(genericServerErrorHandler)
 
 server.listen(PORT,()=>{console.log(`SERVER OK - PORT: ${PORT}`)})
 server.on('error',(error)=>console.log(`SERVER CRASH - ERROR: ${error}`))
