81913f92a9c4efbd074457d2d96f2113f9ae2ea2
marcosaccarola <marco.sacca.ads@gmail.com>
2021-09-20 11:36:05 +0200

    POST review from id (with an hardcoded product example inside products.json

--- src/reviews/index.js
+++ src/reviews/index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import {fileURLToPath} from 'url'
 import path,{dirname} from 'path'
 import fs from 'fs'
+import uniqid from 'uniqid'
+import { send } from 'process'
 
 const _FILENAME=fileURLToPath(import.meta.url)
 const _DIRNAME=dirname(_FILENAME)
@@ -19,6 +21,26 @@ router.get('/',async(req,res,next)=>{
     } catch (error) {
         res.status(500).send({message:error.message})
     }
+})
+// POST A REVIEW by product ID
+router.post('/:id',async(req,res,next)=>{
+    try {        
+        const buffer=fs.readFileSync(reviewsJSONFILEPath)
+        const string=buffer.toString()
+        const array=JSON.parse(string)
+        const{comment,rate}=req.body
+        const review={
+            _id:uniqid(),
+            comment,rate,
+            productId:req.params.id,
+            createdAt:new Date()
+        }
+        array.push(review)
+        fs.writeFileSync(reviewsJSONFILEPath,JSON.stringify(array))
+        res.send(review)
+    } catch (error) {
+        res.status(500).send({message:error.message})
+    }   
 })
 
 export default router
