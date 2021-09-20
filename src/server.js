5eaa0a17481e25186fa4f1dbc643c8edd7fd2ad6
marcosaccarola <marco.sacca.ads@gmail.com>
2021-09-20 10:41:51 +0200

    starting point

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const server=express()
-const PORT=3006
+const PORT=3001
 
 server.listen(PORT,()=>{console.log(`SERVER OK - PORT: ${PORT}`)})
 server.on('error',(error)=>console.log(`SERVER CRASH - ERROR: ${error}`))
