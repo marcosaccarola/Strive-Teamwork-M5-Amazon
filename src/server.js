793b0016ec5973963b6bcc192470193c2bb02099
Sai Krishna Goka <gskr1803@gmail.com>
2021-09-20 15:11:47 +0100

    Image upload done

--- src/server.js
+++ src/server.js
@@ -2,12 +2,15 @@ import express from "express";
 import productsRouter from "./products/index.js";
 import listEndpoints from "express-list-endpoints"
 import cors from "cors"
+import { publicFolderPath } from "../utils/utils.js";
 
 const server = express();
 const PORT = 3001;
 
 server.use(cors())
 server.use(express.json())
+
+server.use(express.static(publicFolderPath))
 server.use("/products", productsRouter)
 
 console.table(listEndpoints(server))
