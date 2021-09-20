fa62732106cfe9df1975147bbe17f42f03a059fe
marcosaccarola <marco.sacca.ads@gmail.com>
2021-09-20 11:52:09 +0200

    PUT update a review by id

--- src/reviews/index.js
+++ src/reviews/index.js
@@ -3,7 +3,7 @@ import {fileURLToPath} from 'url'
 import path,{dirname} from 'path'
 import fs from 'fs'
 import uniqid from 'uniqid'
-import { send } from 'process'
+//import { send } from 'process'
 
 const _FILENAME=fileURLToPath(import.meta.url)
 const _DIRNAME=dirname(_FILENAME)
@@ -41,6 +41,23 @@ router.post('/:id',async(req,res,next)=>{
     } catch (error) {
         res.status(500).send({message:error.message})
     }   
+})
+// UPDATE A REVIEW by review ID
+router.put('/:id',async(req,res,next)=>{
+    try {
+        const buffer=fs.readFileSync(reviewsJSONFILEPath)
+        const string=buffer.toString()
+        const array=JSON.parse(string)
+        const oldReviewIndex=array.findIndex(r=>r.id===req.params.id)
+        if(oldReviewIndex==-1){message:`REVIEW WITH ID ${req.params.id} DOES NOT EXIST`}
+        const oldReview=array[oldReviewIndex]
+        const updatedReview={...oldReview,...req.body,upDatedAt:new Date(),id:req.params.id}
+        array[oldReviewIndex]=updatedReview
+        fs.writeFileSync(reviewsJSONFILEPath,JSON.stringify(array))
+        res.send(updatedReview)
+    } catch (error) {
+        res.status(500).send({message:error.message})
+    }
 })
 
 export default router
